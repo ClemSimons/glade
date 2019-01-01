48984cb7da4fdec830ac8d23d2e3b1c94bb2f76e
Clément Simons <clementsimons@gmail.com>
2019-01-01 09:29:12 +0100

    Add LandingController and views

---
+++ test/controllers/landing_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class LandingControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get Landing_index_url
+    assert_response :success
+  end
+
+end
